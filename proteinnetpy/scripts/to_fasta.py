e0ad2abac8a6562e966e8b0e5304a2df59ff6553
allydunham <ally@ebi.ac.uk>
2020-08-10 17:54:37 +0100

    Added script to convert ProteinNet to fasta#

---
+++ proteinnetpy/scripts/to_fasta.py
@@ -0,0 +1,29 @@
+"""
+Generate a fasta file from a ProteinNet text data file
+"""
+import sys
+import argparse
+import textwrap
+
+from proteinnetpy.parser import record_parser
+
+def parse_args():
+    """Process input arguments"""
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('pn_file', metavar='P', help="Input ProteinNet text file")
+
+    return parser.parse_args()
+
+def main():
+    """Main script"""
+    args = parse_args()
+
+    wrapper = textwrap.TextWrapper(width=60, )
+    for record in record_parser(args.pn_file):
+        print('>', record.id, sep='')
+        print(*wrapper.wrap(''.join(record.primary)), sep='\n')
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='proteinnetpy',
-      version="0.4.0",
+      version="0.4.1",
       description="A python library for working with ProteinNet data",
       url='https://github.com/allydunham/proteinnetpy',
       author='Alistair Dunham',
@@ -11,6 +11,7 @@ setup(name='proteinnetpy',
       install_requires=['numpy', 'pandas', 'biopython'],
       extras_require={'datasets': 'tensorflow>=2.0'},
       entry_points = {
-        'console_scripts': ['add_angles_to_proteinnet=proteinnetpy.add_angles:main']
+        'console_scripts': ['add_angles_to_proteinnet=proteinnetpy.scripts.add_angles:main',
+                            'proteinnet_to_fasta=proteinnetpy.scripts.to_fasta:main']
       },
       zip_safe=True)
